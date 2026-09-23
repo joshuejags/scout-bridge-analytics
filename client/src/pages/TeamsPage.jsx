13db8af0495ac2f0cfffb9d7a68f9743031094c8
Adeolu Shobowale <52972889+joshuejags@users.noreply.github.com>
2026-09-23 09:15:59 +0100

    Remove unused team page memo import after pagination refactor

--- client/src/pages/TeamsPage.jsx
+++ client/src/pages/TeamsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Toast from '../components/Toast';
