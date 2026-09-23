532900accc8e19a24718438dc1f82198675f332a
Adeolu Shobowale <52972889+joshuejags@users.noreply.github.com>
2026-09-23 11:24:52 +0100

    Add jsdom matchMedia test shim

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -13,3 +13,18 @@ jest.mock('socket.io-client', () => ({
     disconnect: jest.fn(),
   })),
 }));
+
+// Browser APIs used by the workspace shell are not provided by jsdom.
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
